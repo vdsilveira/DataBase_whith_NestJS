1d767df2937b34d2f3e706fc0863bdc236ccf33e
Vinicius Duarte Silveira <viniciusd@arklok100.aquarius.cpqd.com.br>
2024-04-18 11:09:06 -0300

    commit

--- dist/main.js
+++ dist/main.js
@@ -4,7 +4,7 @@ const core_1 = require("@nestjs/core");
 const app_module_1 = require("./app.module");
 const common_1 = require("@nestjs/common");
 async function bootstrap() {
-    const app = await core_1.NestFactory.create(app_module_1.AppModule);
+    const app = await core_1.NestFactory.create(app_module_1.AppModule, { cors: true });
     app.useGlobalPipes(new common_1.ValidationPipe({
         transform: true,
         whitelist: true,
